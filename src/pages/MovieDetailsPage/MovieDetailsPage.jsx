2b74889cd241626674d6ee8af98b381045a1aa36
Roman_Firtsovych <r.firtsovych@gmail.com>
2025-01-18 21:26:19 +0200

    fatality

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -18,7 +18,7 @@ const MovieDetailsPage = () => {
   }, [movieId]);
 
   const handleGoBack = () => {
-    navigate(location.state?.from ?? '/movies');
+    navigate(location.state?.from ?? '/');
   };
 
   const handleWatchTrailer = () => {
